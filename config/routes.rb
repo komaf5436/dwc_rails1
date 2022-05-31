23e90660a97347d827b49a8781e87b9f9f2a20f7
komaf5436 <k.3624.t.aw@gmail.com>
2022-05-31 10:29:58 +0000

    topルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # HTTPメソッド 'URL' => 'コントローラ#アクション'
+  # URL「top」にアクセスした時にhomesコントローラのtopアクションが呼び出される
+  get 'top' => 'homes#top'
 end
