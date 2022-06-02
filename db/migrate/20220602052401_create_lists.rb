c5f5b40b5fe0224bb6fcb91338f4cb98be3f13e6
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-02 05:46:39 +0000

    migrationファイル作成・実行

--- db/migrate/20220602052401_create_lists.rb
+++ db/migrate/20220602052401_create_lists.rb
@@ -1,6 +1,10 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
+      
+      # t.データ型 :カラム名
+      t.string :title
+      t.string :body
 
       t.timestamps
     end
